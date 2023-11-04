572afb909fab1837c1fee7c2313f26d1fbb92d64
Ruijie Fan <73094393+FM3676@users.noreply.github.com>
2023-11-04 11:55:00 +0800

    Create 104.二叉树的最大深度.js

---
+++ 104.二叉树的最大深度.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode.cn id=104 lang=javascript
+ *
+ * [104] 二叉树的最大深度
+ *
+ * https://leetcode.cn/problems/maximum-depth-of-binary-tree/description/
+ *
+ * algorithms
+ * Easy (77.19%)
+ * Likes:    1734
+ * Dislikes: 0
+ * Total Accepted:    1.1M
+ * Total Submissions: 1.5M
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * 给定一个二叉树 root ，返回其最大深度。
+ *
+ * 二叉树的 最大深度 是指从根节点到最远叶子节点的最长路径上的节点数。
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ *
+ *
+ *
+ *
+ * 输入：root = [3,9,20,null,null,15,7]
+ * 输出：3
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：root = [1,null,2]
+ * 输出：2
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * 树中节点的数量在 [0, 10^4] 区间内。
+ * -100 <= Node.val <= 100
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function (root) {
+  let result = 0;
+  const dfs = (root, current) => {
+    if (root === null) return;
+    if (current + 1 > result) result = current + 1;
+    dfs(root.left, current + 1);
+    dfs(root.right, current + 1);
+  };
+  dfs(root, result);
+  return result;
+};
+// @lc code=end
