353beb6c8e1062f4c18c2de1a133eca1e3ea7c32
Ruijie Fan <73094393+FM3676@users.noreply.github.com>
2023-11-09 21:36:59 +0800

    Create 454.四数相加-ii.js

---
+++ 454.四数相加-ii.js
@@ -0,0 +1,92 @@
+/*
+ * @lc app=leetcode.cn id=454 lang=javascript
+ *
+ * [454] 四数相加 II
+ *
+ * https://leetcode.cn/problems/4sum-ii/description/
+ *
+ * algorithms
+ * Medium (64.24%)
+ * Likes:    920
+ * Dislikes: 0
+ * Total Accepted:    233.5K
+ * Total Submissions: 363.4K
+ * Testcase Example:  '[1,2]\n[-2,-1]\n[-1,2]\n[0,2]'
+ *
+ * 给你四个整数数组 nums1、nums2、nums3 和 nums4 ，数组长度都是 n ，请你计算有多少个元组 (i, j, k, l)
+ * 能满足：
+ *
+ *
+ * 0 <= i, j, k, l < n
+ * nums1[i] + nums2[j] + nums3[k] + nums4[l] == 0
+ *
+ *
+ *
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：nums1 = [1,2], nums2 = [-2,-1], nums3 = [-1,2], nums4 = [0,2]
+ * 输出：2
+ * 解释：
+ * 两个元组如下：
+ * 1. (0, 0, 0, 1) -> nums1[0] + nums2[0] + nums3[0] + nums4[1] = 1 + (-2) +
+ * (-1) + 2 = 0
+ * 2. (1, 1, 0, 0) -> nums1[1] + nums2[1] + nums3[0] + nums4[0] = 2 + (-1) +
+ * (-1) + 0 = 0
+ *
+ *
+ * 示例 2：
+ *
+ *
+ * 输入：nums1 = [0], nums2 = [0], nums3 = [0], nums4 = [0]
+ * 输出：1
+ *
+ *
+ *
+ *
+ * 提示：
+ *
+ *
+ * n == nums1.length
+ * n == nums2.length
+ * n == nums3.length
+ * n == nums4.length
+ * 1 <= n <= 200
+ * -2^28 <= nums1[i], nums2[i], nums3[i], nums4[i] <= 2^28
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @param {number[]} nums3
+ * @param {number[]} nums4
+ * @return {number}
+ */
+var fourSumCount = function (nums1, nums2, nums3, nums4) {
+  const twoSumMap = new Map();
+  let count = 0;
+
+  for (const n1 of nums1)
+    for (const n2 of nums2) {
+      const sum = n1 + n2;
+      twoSumMap.set(sum, (twoSumMap.get(sum) || 0) + 1);
+    }
+
+  for (const n3 of nums3)
+    for (const n4 of nums4) {
+      const sum = n3 + n4;
+      count += twoSumMap.get(0 - sum) || 0;
+    }
+
+  return count;
+  /* 
+作者：代码随想录
+链接：https://leetcode.cn/problems/4sum-ii/solutions/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。 */
+};
+// @lc code=end
